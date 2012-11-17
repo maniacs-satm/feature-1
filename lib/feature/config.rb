1daf6640bf3a8c1182dbd77f1ee5e29f9cc440a5
Alan Kennedy <alankennedy100@gmail.com>
2012-11-17 13:24:20 +0000

    config can add groups

--- lib/feature/config.rb
+++ lib/feature/config.rb
@@ -1,11 +1,15 @@
+require 'set'
+
 # Class for the Feature configuration DSL. Methods defined here are
 # available within the block passed to Feature.configure
 class Feature::Config
   attr_reader :features
+  attr_reader :groups
   attr_reader :backend_obj
 
   def initialize(block = nil)
     @features = {}
+    @groups = Set.new
     instance_eval &block unless block.nil?
   end
 
@@ -14,6 +18,13 @@ class Feature::Config
     raise "Feature '#{name}' already exists" if @features.include?(name)
     opts[:default] = true unless opts.include?(:default)
     @features[name] = opts
+  end
+
+  def group(name, opts = {})
+    values = opts.delete(:values) || []
+
+    @groups << name
+    @backend_obj.new_group(name, values)
   end
 
   def backend(backend)
